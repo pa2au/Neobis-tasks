6b0a9fd6c1cc877aef8c64d28e44d7ffdb03248f
Ermek Ashirov <ashirovermek00@gmail.com>
2021-05-16 17:07:20 +0600

    URI 1253 Solve

---
+++ week_2/Caesar_Cipher.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Caesar_Cipher{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for (int i = 0; i < N; i++) {
+           String text=in.next();
+           int T= in.nextInt();
+           char cipher[]=text.toCharArray();
+
+           for (int j=0;j<cipher.length;j++){
+               char c = cipher[j];
+               if (c >= 65 && c <= 90)
+               {
+                   int x = c - 65;
+                   x = (x - T) % 26;
+                   if (x < 0)
+                       x += 26;
+                   cipher[j] = (char) (x + 65);
+               }
+           }
+            System.out.println(cipher);
+        }
+    }
+}
