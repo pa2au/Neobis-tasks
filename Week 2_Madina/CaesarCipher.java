39c5f35dfcae88ac8aa1eefccb49c99836f1626c
MadinaTanikulova <maditanigallery2@gmail.com>
2021-05-19 16:39:21 +0600

    Initial commit.

---
+++ Week 2_Madina/CaesarCipher.java
@@ -0,0 +1,26 @@
+/*
+ *javac -d . *.java
+ *java Caesar_Cipher
+ */
+
+import java.util.Scanner;
+
+public class CaesarCipher {
+    public static void main( String[] args ) {
+	Scanner scanner = new Scanner(System.in);
+	int N = Integer.parseInt( scanner.nextLine() );	
+	    for (int k = N; k > 0; k--) {
+	       String text = scanner.nextLine();
+  	       int  p = Integer.parseInt(scanner.nextLine());
+	       String result = "";                           
+	      for(int i = 0; i < text.length(); i++) {
+		if ('A' <= text.charAt(i) - p){
+	            result += (char) (text.charAt(i) - p);
+		}else{
+	           result += (char) (('Z' - ('A' -  (text.charAt(i) - p))) + 1);   
+		}
+	      } 		      	           
+	    System.out.println(result);
+	    }
+    }
+}
