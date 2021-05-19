6a4d95958f8d2b7433710718ed684bd8ccd1b9b6
MadinaTanikulova <83603205+MadinaTanikulova@users.noreply.github.com>
2021-05-19 16:56:55 +0600

    Add files via upload

---
+++ Week 2_Madina/FlaviousJosephusLegend.java
@@ -0,0 +1,27 @@
+/*
+ *javac -d . *.java
+ *
+ *java Flavious_Josephus_Legend
+ *
+ */
+
+import java.util.Scanner;
+
+public class FlaviousJosephusLegend {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+            for (int i = 1; i <= N; i++) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                    int result =Result(x, y) + 1;
+                    System.out.println("Case " + i + ": " +result);
+                }
+    }
+    public static int Result(int x, int y) {
+        int result = 0;
+        for (int i = 2; i <= x; i++)
+            result= (result + y) % i;
+        return result;
+    }
+}
